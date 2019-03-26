d0b17e5e9a6551f8eb2b2a5eb1b81b421de55d24
Oleksandr Fediashov <olfedias@microsoft.com>
2019-03-26 14:33:38 +0200

    wip

--- docs/src/examples/components/Box/Performance/ContextConsumer.perf.tsx
+++ docs/src/examples/components/Box/Performance/ContextConsumer.perf.tsx
@@ -1,4 +1,5 @@
 import { ThemeInput } from '@stardust-ui/react'
+import * as _ from 'lodash'
 import * as React from 'react'
 // @ts-ignore
 import { ThemeContext } from 'react-fela'
@@ -11,115 +12,9 @@ const ContextConsumer = () => (
 
 const ContextConsumerPerf = () => (
   <>
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
-    <ContextConsumer />
+    {_.times(1000, key => (
+      <ContextConsumer key={key} />
+    ))}
   </>
 )
 
